283a5067e887294a8cfd69904defc13a58e96b10
kkooHan1124 <iaw941224@gmail.com>
2022-09-20 14:47:02 +0900

    Initial commit

---
+++ NamingVariableAndConstant/src/sec01_datatype/EX02_NamingVariableAndConstant/NamingVariableAndConstant.java
@@ -0,0 +1,25 @@
+package sec01_datatype.EX02_NamingVariableAndConstant;
+
+public class NamingVariableAndConstant {
+	public static void main(String[] args) {
+		// 변수
+		boolean aBcD;
+		byte 가나다;
+		short _abcd;
+		char $ab_cd;
+		// int 3abcd;
+		long abcd3;
+		// float int;
+		double main;
+		// int my Work;
+		String myClassName;
+		int ABC;
+		
+		
+		// 상수
+		final double PI;
+		final int MY_DATA;
+		final float myData;		// 소문자 사용(권장하지 않음)
+	}
+
+}
